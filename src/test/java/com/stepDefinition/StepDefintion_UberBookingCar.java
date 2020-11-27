e5ecaa360588e4121f1a2eb40a0b550793d08530
VENKAT <konduvenkatarao999@gmail.com>
2020-11-27 10:05:53 +0530

    ADD some update if current files

--- src/test/java/com/stepDefinition/StepDefintion_UberBookingCar.java
+++ src/test/java/com/stepDefinition/StepDefintion_UberBookingCar.java
@@ -5,31 +5,29 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefintion_UberBookingCar {
+	@Given("User wants book a car type like {string} from uber app #How to pass the values form this place to glue code How")
+	public void user_wants_book_a_car_type_like_from_uber_app_how_to_pass_the_values_form_this_place_to_glue_code_how(String carType) {
+	  System.out.println("Step - 1 : "+carType);
+	}
 
-@Given("User wants book a car type like {string} from uber app")
-public void user_wants_book_a_car_type_like_from_uber_app(String carType) {
-	System.out.println("Step -1 - User wants a book a car form This Uber App And car Type : "+carType);
-}
+	@When("User selects a car {string}  and pick up Location {string} and drop Location {string}")
+	public void user_selects_a_car_and_pick_up_location_and_drop_location(String carType, String picLocation, String dropLocation) {
+	    System.out.println("Step -2 - CarType :\'"+carType+"\' pikUp Location From :\'"+picLocation+"\' And Drop Location At :\'"+dropLocation+"\'");
+	}
 
-@When("User selects a car {string}  and pick up Location {string} and drop Location {string}")
-public void user_selects_a_car_and_pick_up_location_and_drop_location(String carType, String pic_Location, String drop_Location) {
-	System.out.println("Step -2 - user Selects a picup Location  :\'"+carType+"\' And Puk Up Loaction \'"+pic_Location+"\' And Drop Location \'"+drop_Location+"\' ");
-}
+	@Then("Driver Starts the Journey from {string}")
+	public void driver_st_nrats_the_journey_from(String picLocation) {
+	    System.out.println("Step -3 - Driver Starts Journey At :\'"+picLocation+"\'");
+	}
 
-@Then("Driver Strats the Journey")
-public void driver_strats_the_journey() {
-	System.out.println("Step -3 - Driver Starts the Journey From :pikUpLoaction");
-}
+	@Then("Driver End the Journey At {string}")
+	public void driver_end_the_journey_at(String dropJourney) {
+	    System.out.println("Step -4 - Driver Ends the Journey At :\'"+dropJourney+"\'");
+	}
 
-@Then("Driver End the Journey")
-public void driver_end_the_journey() {
-	System.out.println("Step -2 - Driver Ends the Journey At : DropLocation");
-}
-
-@Then("User pay {int} IND")
-public void user_pay_ind(Integer rootCost) {
-	System.out.println("Step -2 - User Pay the payment in Indian Rupies Rs :"+rootCost+"Rs\\-");
-}
-
+	@Then("User pay {string} IND")
+	public void user_pay_ind(String rootCost) {
+		System.out.println("Step -5 - User pay the Amout to the Application : \'"+rootCost+"\'");
+	}
 
 }
--- target/classes/appFeatures/uber.feature
+++ target/classes/appFeatures/uber.feature
@@ -1,12 +1,21 @@
+
+# How to pass the values form this feature file to glue code how it is possible 
+
+#String carType="Tufan";
+#String picUpLocation="Hyderabad";
+#String dropLocation="Banglore"
+#int rootCost=1000;
+
+
 Feature: Uber Car Booking 
 
 Scenario: Booking a Cab
 
 # for this feature there is no glue code is abailable how to implement Glue code for this feature 
 #																		{ "String "} -- this is called Reguler Expression for feture file 
-Given User wants book a car type like "Tufan" from uber app
-When User selects a car "Tufan"  and pick up Location "Hyderabad" and drop Location "Bengalore"
-Then Driver Strats the Journey
-And Driver End the Journey
-Then User pay 500 IND  
+Given User wants book a car type like "+carType+" from uber app #How to pass the values form this place to glue code How 
+When User selects a car "+carType+"  and pick up Location "pikUpLocation" and drop Location "+dropLocation+"
+Then Driver Starts the Journey from "+pikUpLocation+"
+And Driver End the Journey At "+dropLocation+"
+Then User pay "+rootCost+" IND  
 
